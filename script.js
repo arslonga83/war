86412b95d7d177d340bc5cfcfa0daab4a29612bd
arslonga83 <jlwood83@gmail.com>
2023-01-02 15:01:03 -0800

    display cards after drawing

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ function draw2() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-      console.log(data.cards[0])
-      console.log(data.cards[1])
+      document.querySelector('#img-container').innerHTML = `
+      <img src=${data.cards[0].images.png}>
+      <img src=${data.cards[1].images.png}>
+      `
     })
 }
+
 
 document.querySelector('#newDeckBtn').addEventListener('click', getDeck)
 document.querySelector('#drawBtn').addEventListener('click', draw2)
