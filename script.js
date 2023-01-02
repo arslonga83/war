e11b49746c949c8756d1bcccfa7ce3fa06f23163
arslonga83 <jlwood83@gmail.com>
2023-01-02 14:53:37 -0800

    add function to draw 2 and log them to console

---
+++ script.js
@@ -0,0 +1,19 @@
+let deckId = ''
+  
+function getDeck() {
+  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+  .then(res => res.json())
+  .then(data => deckId = data.deck_id)
+}
+
+function draw2() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.cards[0])
+      console.log(data.cards[1])
+    })
+}
+
+document.querySelector('#newDeckBtn').addEventListener('click', getDeck)
+document.querySelector('#drawBtn').addEventListener('click', draw2)
