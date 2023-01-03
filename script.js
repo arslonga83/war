d86c1486ad1ac91df0491a508e469e2f4a5ef353
arslonga83 <jlwood83@gmail.com>
2023-01-02 21:11:20 -0800

    add function to determine winner of each hand and display result

--- script.js
+++ script.js
@@ -16,7 +16,31 @@ function draw2() {
       document.querySelector('#cardSlot2').innerHTML = `
       <img src=${data.cards[1].image}>
       `
+      document.querySelector('#result').innerHTML = getWinner(data.cards[0], data.cards[1])
     })
+}
+
+function getWinner(card1, card2) {
+  const cardKey = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'JACK': 11,
+    'QUEEN': 12,
+    'KING': 13,
+    'ACE': 14
+  }
+  if (cardKey[card1.value] > cardKey[card2.value]) {
+    return 'Computer Wins'
+  } else if (cardKey[card1.value] < cardKey[card2.value]) {
+    return 'You Win'
+  } else return 'War!'
 }
 
 document.querySelector('#shuffleBtn').addEventListener('click', getDeck)
