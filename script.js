8865342ff204930909ee054ccfdc9a46af801bc8
arslonga83 <jlwood83@gmail.com>
2023-01-03 21:32:19 -0800

    display final winner. refactor code to be a bit more dry

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let deckId = ''
 const shuffleBtn = document.querySelector('#shuffleBtn')
 const drawBtn = document.querySelector('#drawBtn')
+const result = document.querySelector('#result')
+const cardSlot1 = document.querySelector('#cardSlot1')
+const cardSlot2 = document.querySelector('#cardSlot2')
+const score1 = document.querySelector('#score1')
+const score2 = document.querySelector('#score2')
+const remainingCards = document.querySelector('#remainingCards')
+
+let deckId = ''
 const scores = {
   player1: 0,
   player2: 0
 }
 
-  
 function getDeck() {
   fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
   .then(res => res.json())
   .then(data => {
     deckId = data.deck_id
-    document.querySelector('#remainingCards').innerHTML = `Remaining Cards: ${data.remaining}`
+    remainingCards.innerHTML = `Remaining Cards: ${data.remaining}`
     drawBtn.disabled = false;
     shuffleBtn.disabled = true;
-    document.querySelector('#result').innerHTML = ``
+    result.innerHTML = ``
+    cardSlot1.innerHTML = ``
+    cardSlot2.innerHTML = ``
+    score1.textContent = `Player 1 - 0 wins`
+    score2.textContent = `Player 2 - 0 wins`
   })
 }
 
@@ -23,31 +33,20 @@ function draw2() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-      console.log(data)
-      if (data.success) {
-        document.querySelector('#cardSlot1').innerHTML = `
+        cardSlot1.innerHTML = `
         <img src=${data.cards[0].image}>`
-        document.querySelector('#cardSlot2').innerHTML = `
+        cardSlot2.innerHTML = `
         <img src=${data.cards[1].image}>
         `
-        document.querySelector('#result').innerHTML = getWinner(data.cards[0], data.cards[1])
-        document.querySelector('#remainingCards').innerHTML = `Remaining Cards: ${data.remaining}`
-      } 
-      else {
+        result.innerHTML = getWinner(data.cards[0], data.cards[1])
+        remainingCards.textContent = `Remaining Cards: ${data.remaining}`
+        score1.textContent = `Player 1 - ${scores.player1} wins`
+        score2.textContent = `Player 2 - ${scores.player2} wins`
+       
+      if (data.remaining === 0) {
         drawBtn.disabled = true;
         shuffleBtn.disabled = false;
-        document.querySelector('#cardSlot1').innerHTML = ``
-        document.querySelector('#cardSlot2').innerHTML = ``
-        
-        let winner = ''
-        if (scores.player1 > scores.player2) {
-          winner = 'Player 1'
-        } if (scores.player1 < scores.player2) {
-          winner = 'Player 2'
-        }
-        document.querySelector('#result').innerHTML = `${
-          scores.player1 === scores.player2 ? 'Tie Game' : winner + ' wins!'
-        }`
+        getFinalResult()
       }
     })
 }
@@ -68,14 +67,25 @@ function getWinner(card1, card2) {
     'KING': 13,
     'ACE': 14
   }
-
   if (cardKey[card1.value] > cardKey[card2.value]) {
     scores.player1++
-    return 'Card 1 Wins'
+    return `${card1.value} Wins`
   } else if (cardKey[card1.value] < cardKey[card2.value]) {
     scores.player2++
-    return 'Card 2 Wins'
+    return `${card2.value} Wins`
   } else return 'War!'
+}
+
+function getFinalResult() {
+  let winner = ''
+        if (scores.player1 > scores.player2) {
+          winner = 'Player 1'
+        } if (scores.player1 < scores.player2) {
+          winner = 'Player 2'
+        }
+        result.innerHTML = `${
+          scores.player1 === scores.player2 ? 'Tie Game' : winner + ' wins!'
+        }`
 }
 
 shuffleBtn.addEventListener('click', getDeck)
